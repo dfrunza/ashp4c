e427f84f704a9a2db29d0738bd6d35641a5c2a12
Dumitru Frunza <dumitru.frunza@gmail.com>
2021-07-11 13:56:31 -0400

    run_tests.sh: ignore the `include` dir.

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,7 @@
 reset;
-for f in `ls testdata`; do \
+for f in `find testdata -maxdepth 1 -type f`; do \
     echo;
-    ./build/dp4c testdata/$f;
+    ./build/dp4c $f;
     if [ $? -eq 0 ]; then
         echo "$f : PASSED"
     else
