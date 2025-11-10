e8424adf18894078bc2fe0b7357c781e6ade98fb
Dumitru Frunza <dumitru.frunza@gmail.com>
2025-11-10 12:44:32 -0500

    build.sh : Don't use freestanding mode.

--- build.sh
+++ build.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-# -std=gnu99  ...  Access to the GCC extension 'Referring to a Type with typeof'.
-
 CC=""
-CC="$CC -g3 -std=c++11 -ggdb -fpermissive -fms-extensions -ffreestanding"
-CC="$CC -Wno-pointer-arith -Wno-sign-compare -Wno-nonnull-compare -Winline -Wno-write-strings -Wno-unused-function -Wreturn-type -Wall"
+CC="$CC -g3 -std=c++11 -ggdb -fpermissive -fms-extensions"
+CC="$CC -Wno-pointer-arith -Wno-sign-compare -Wno-nonnull-compare -Winline -Wno-write-strings"
+CC="$CC -Wno-format -Wno-unused-function -Wreturn-type -Wall"
 
 g++ $CC -o ashp4c \
   ashp4c.cpp basic.cpp arena.cpp array.cpp strmap.cpp map.cpp \
