90a24840ddbd7e3aa8f8e35206a976039a470ebc
Dumitru Frunza <dumitru.frunza@gmail.com>
2026-01-03 13:27:05 -0500

    #2: Separate template decl from def.

--- array.h
+++ array.h
@@ -48,49 +48,8 @@ struct Array {
   int capacity;
   SegmentTable<T> elements;
 
-  static Array* create(Arena* storage, int segment_count)
-  {
-    assert(segment_count >= 1 && segment_count <= 16);
-
-    Array* array = storage->allocate<Array>();
-    storage->allocate<T**>(segment_count);
-    array->storage = storage;
-    array->element_count = 0;
-    array->capacity = 16;
-    array->elements.segment_count = segment_count;
-    array->elements.segments[0] = storage->allocate<T>(16);
-    return array;
-  }
-
-  void extend()
-  {
-    assert(element_count >= capacity);
-
-    int last_segment = floor(log2(capacity/16 + 1));
-    if (last_segment >= elements.segment_count) {
-      printf("\nMaximum array capacity has been reached.\n");
-      exit(1);
-    }
-    int segment_capacity = 16 * (1 << last_segment);
-    elements.segments[last_segment] = storage->allocate<T>(segment_capacity);
-    capacity = 16 * ((1 << (last_segment + 1)) - 1);
-  }
-
-  T* get(int i)
-  {
-    assert(i >= 0 && i < element_count);
-
-    T* elem_slot = elements.locate_cell(i);
-    return elem_slot;
-  }
-
-  T* append()
-  {
-    if (element_count >= capacity) {
-      extend();
-    }
-    T* elem_slot = elements.locate_cell(element_count);
-    element_count += 1;
-    return elem_slot;
-  }
+  static Array* create(Arena* storage, int segment_count);
+  void extend();
+  T* get(int i);
+  T* append();
 };
